bf9eb0157ef7bcb784aca84ed1c406ed6124572c
pabad-ap42M <pabad-ap@student.42madrid.com>
2025-02-07 15:22:24 +0100

    Class templates webserv.conf con algun extra

---
+++ templates/errors_class.hpp
@@ -0,0 +1,5 @@
+/**
+ * Clase con la estructura principal de error
+ * y todos los errores, Un string por defecto si no hay pagina indicada,
+ * o el fichero con la pagina de error indicada.
+ */
---
+++ templates/server_class.hpp
@@ -0,0 +1,5 @@
+/**
+ * Plantilla de parametros y funciones que pueden manejar los servidores
+ * - 1 puerto por listen, aunque puede haber varios listen por server
+ */
+
